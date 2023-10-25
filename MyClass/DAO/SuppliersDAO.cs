885dcec1499f0972d5223d1b9f60bec94584d9d2
phattranduy <115158729+DuyPhatpeo@users.noreply.github.com>
2023-10-25 21:09:41 +0700

    SuppliersDAO

---
+++ MyClass/DAO/SuppliersDAO.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using MyClass.Model;
+
+namespace MyClass.DAO
+{
+
+    public class SuppliersDAO
+    {
+        private MyDBContext db = new MyDBContext();
+
+        public List<Suppliers> getlist()
+        {
+            return db.Suppliers.ToList();
+        }
+        // INDEX dua vao Status = 1,2, con Status = 0 == thung rac
+        public List<Suppliers> getList(string status = "ALL")
+        {
+            List<Suppliers> list = null;
+            switch (status)
+            {
+                case "Index":
+                    {
+                        list = db.Suppliers
+                            .Where(m => m.Status != 0)
+                            .ToList();
+                        break;
+                    }
+                case "Trash":
+                    {
+                        list = db.Suppliers
+                            .Where(m => m.Status == 0)
+                            .ToList();
+                        break;
+                    }
+                default:
+                    {
+                        list = db.Suppliers.ToList();
+                        break;
+                    }
+            }
+            return list;
+        }
+
+        //DETAILS
+        public Suppliers getRow(int? id)
+        {
+            if (id == null)
+            {
+                return null;
+            }
+            else
+            {
+                return db.Suppliers.Find(id);
+            }
+        }
+
+        //CREATE
+        public int Insert(Suppliers row)
+        {
+            db.Suppliers.Add(row);
+            return db.SaveChanges();
+        }
+
+        //UPDATE
+        public int Update(Suppliers row)
+        {
+            db.Entry(row).State = EntityState.Modified;
+            return db.SaveChanges();
+        }
+
+        //DELETE
+        public int Delete(Suppliers row)
+        {
+
+            db.Suppliers.Remove(row);
+            return db.SaveChanges();
+        }
+    }
+
+}
