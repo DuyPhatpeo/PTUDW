4610cfceac169fe19ccd7fce580b60289488b0ee
phattranduy <115158729+DuyPhatpeo@users.noreply.github.com>
2023-11-27 13:39:42 +0700

    Main menu

---
+++ ThucHanh/Controllers/ModuleController.cs
@@ -0,0 +1,26 @@
+﻿using MyClass.DAO;
+using MyClass.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace ThucHanh.Controllers
+{
+    public class ModuleController : Controller
+    {
+        MenusDAO menusDAO = new MenusDAO();
+        // GET: Module
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MainMenu() 
+        {
+            List<Menus> list = menusDAO.getListByParentId(0);
+            return View(list);
+        }
+    }
+}
