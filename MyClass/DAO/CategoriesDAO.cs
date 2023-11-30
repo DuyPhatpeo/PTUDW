006b62f4f0c675183723ebb3123734b16eec6748
phattranduy <115158729+DuyPhatpeo@users.noreply.github.com>
2023-11-30 19:39:33 +0700

    Categories

--- MyClass/DAO/CategoriesDAO.cs
+++ MyClass/DAO/CategoriesDAO.cs
@@ -14,41 +14,43 @@ namespace MyClass.DAO
     public class CategoriesDAO
     {
         private MyDBContext db = new MyDBContext();
-
-        public List<Categories> getlist()
+        public List<Categories> getList()
         {
             return db.Categories.ToList();
         }
-        // INDEX dua vao Status = 1,2, con Status = 0 == thung rac
-        public List<Categories> getList(string status = "ALL")
+        // GET: Admin/Category
+        public List<Categories> getListByPareantId(int parentid = 0)
+        {
+            return db.Categories
+                .Where(m => m.ParentID == parentid && m.Status == 1)
+                .OrderBy(m => m.Order)
+                .ToList();
+        }
+
+        //INDEX dua vao status = 1,2, còn status = 0 == thung rac
+        public List<Categories> getList(string status = "All")
         {
             List<Categories> list = null;
             switch (status)
             {
                 case "Index":
                     {
-                        list = db.Categories
-                            .Where(m => m.Status !=0)
-                            .ToList ();
+                        list = db.Categories.Where(m => m.Status != 0).ToList();
                         break;
                     }
                 case "Trash":
                     {
-                        list = db.Categories
-                            .Where(m => m.Status == 0)
-                            .ToList();
+                        list = db.Categories.Where(m => m.Status == 0).ToList();
                         break;
                     }
                 default:
                     {
-                        list = db.Categories.ToList ();
-                        break;
+                        return db.Categories.ToList();
                     }
             }
             return list;
         }
-
-        //DETAILS
+        // Details
         public Categories getRow(int? id)
         {
             if (id == null)
@@ -61,21 +63,28 @@ namespace MyClass.DAO
             }
         }
 
-        //CREATE
+        public Categories getRow(string slug)
+        {
+
+            return db.Categories
+                .Where(m => m.Slug == slug && m.Status == 1)
+                .FirstOrDefault();
+        }
+        // Create 
         public int Insert(Categories row)
         {
             db.Categories.Add(row);
             return db.SaveChanges();
-        }
 
-        //UPDATE
+        }
+        //Update
         public int Update(Categories row)
         {
             db.Entry(row).State = EntityState.Modified;
             return db.SaveChanges();
         }
 
-        //DELETE
+        //Delete
         public int Delete(Categories row)
         {
             db.Categories.Remove(row);
